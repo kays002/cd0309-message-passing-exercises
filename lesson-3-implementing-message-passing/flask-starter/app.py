bcc2ffe4028d1749dc118ac1e39b2da12bb1dffd
kays002 <kaushikisingh6920@gmail.com>
2024-08-23 12:46:43 +0530

    added api

--- lesson-3-implementing-message-passing/flask-starter/app.py
+++ lesson-3-implementing-message-passing/flask-starter/app.py
@@ -10,5 +10,12 @@ app = Flask(__name__)
 def health():
     return jsonify({'response': 'Hello World!'})
 
+@app.route('/api/orders/order_data', methods=['GET','POST'])
+def abc(order_data):
+    if request.method=='GET':
+        return Response(json.dumps(retrieve_orders(order_data)), 200, {Content-Type: application/json})
+    if request.method=='POST':
+        return.Response(json.dumps(create_order(order_data)))
+
 if __name__ == '__main__':
     app.run()
